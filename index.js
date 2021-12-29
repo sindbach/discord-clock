4498d59d6f2b7a2aec49e51f5a4416379eb77b76
Wan Bachtiar <sindbach@gmail.com>
2021-12-29 23:46:35 +1100

    Updated to use Discord.js ready event

--- index.js
+++ index.js
@@ -11,19 +11,24 @@ const port = process.env.PORT || 3001;
 
 const { TIMEZONE, FORMAT, CHANNEL_ID, UPDATE_INTERVAL, BOT_TOKEN} = process.env;
 
-app.get("/", (req, res) => res.send("안녕하세요!"));
+app.get("/", (req, res) => {
+  console.log("landing accessed");
+  res.send("안녕하세요!")
+});
 app.get("/update", async (req, res) => {
-  await client.login(BOT_TOKEN);
-  //define clockChannel
-  const clockChannel = await client.channels.cache.get(CHANNEL_ID);
-  //init time
-  const timeNow = moment().tz(TIMEZONE).format(FORMAT);
-  //initial update
-  clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
-    .catch(console.error);
-  //tells if it is ready
-  console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
-  res.send("success");
+  client.login(BOT_TOKEN);
+  client.once('ready', () => {
+    //define clockChannel
+    const clockChannel = client.channels.cache.get(CHANNEL_ID);
+    //init time
+    const timeNow = moment().tz(TIMEZONE).format(FORMAT);
+    //initial update
+    clockChannel.edit({ name: `🕒 ${timeNow}` }, 'Clock update')
+      .catch(console.error);
+    //tells if it is ready
+    console.log(chalk.greenBright("[READY]"), `Logged in as ${client.user.tag} (${client.user.id}) at ${moment().format("DD MMMM YYYY, HH:mm:ss")}`);
+    res.send("success");
+  });
 });
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
